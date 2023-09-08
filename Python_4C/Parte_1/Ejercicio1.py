2676e95646088407d5e51fa4f9aaaf568785103d
al3-rom <sasha.stankevich223@gmail.com>
2023-09-08 18:11:21 +0200

    Repaso con profe

--- Python_4C/Parte_1/Ejercicio1.py
+++ Python_4C/Parte_1/Ejercicio1.py
@@ -18,11 +18,43 @@ peli = np.array([
     ["DoctorStrange", "Comedia", 90, 2022, 10],
 ])
 
-# Analizamos los datos
-genero = peli[:,1]
-lanzamiento = peli[:,3]
-duracion = peli[:,2]
+# Genero mas popular 
 
+# return_counts sirve para contar cuantas veces aparece elemento
+generos, conteos = np.unique(peli[:,1], return_counts = True)
+
+# Ordenamos los conteos de mayor a menor
+conteos_desc = np.argsort(conteos) [::-1] # nos devuelve los indices de los elementos del array conteos de mayor a menor
+                                            # el resultado es [1,2,0]
+
+# extraer el genero mas popular
+genero_popular = generos[conteos_desc[0]] # conteos_desc[0] contiene el indice con el conteo mas alto
+print("El genero mas popular es:", genero_popular)
+
+# Agrupamos las peliculas por decada
+# Creamos array con las dec adas a tratar
+decadas = np.unique(peli[:,3].astype(int) // 10 * 10)
+print(decadas)
+
+# Contamos las peliculas en cada decada
+
+conteos_decadas = []
+for decada in decadas:
+    conteo = np.count_nonzero((peli[:,3].astype(int) >= decada) & (peli[:,3].astype(int) < decada + 10))
+    conteos_decadas.append(conteo)
+    print("En la decada de ", decada, "se crearon", conteo, "peliculas")
+
+# Duracion promedio por genero
+
+todos_generos = peli[:,1]
+duraciones = peli[:,2]
+
+duracion_media = np.zeros(len(generos))
+
+for i in range(len(generos)):
+                                                                                                                                         
+    duracion_media[i] = np.mean(duraciones[todos_generos == generos[i]].astype(float))
+    print("Duracion media de las peliculas de tipo: ", generos[i], "es de", duracion_media[i], "minutos")
 
 
         
--- Python_4C/Parte_2/Ejercicio2.py
+++ Python_4C/Parte_2/Ejercicio2.py
@@ -10,3 +10,41 @@ promedio de cada tipo de componente.
 
 (Pista 1: puede ser util investigar np.unique y np.argmax)
 """
+import numpy as np
+
+datos = np.array([
+    ['2022-01-01', 'Componente 1', "Lote A", 80],
+    ['2022-01-15', 'Componente 2', "Lote B", 95],
+    ['2022-02-01', 'Componente 2', "Lote C", 90],
+    ['2022-02-01', 'Componente 1', "Lote C", 70],
+    ['2022-02-01', 'Componente 1', "Lote C", 90]
+])
+# Identificar el componente con la puntuacion mas alta
+tipos_componente = datos[:,1]
+tipos_unicos = np.unique(tipos_componente)
+calidad = datos[:,3].astype(float)
+promedios = np.zeros(len(tipos_unicos))
+i = 0
+
+for tipo in tipos_unicos:
+   promedios[i] = np.mean(calidad[tipos_componente == tipo])
+   i = i + 1
+
+indice_maximo = np.argmax(promedios)
+tipo_mejor_calidad = tipos_unicos[indice_maximo]
+
+print("El tipo de componente con la puntuacion de calidad mas alta es:", tipo_mejor_calidad)
+
+# cuantos componentes se produjeros en cada mes
+
+fechas = datos[:,0]
+meses, counts = np.unique([fecha[0:7] for fecha in fechas],  return_counts= True)
+print(meses, counts)
+
+# Puntuacion de calidad promedio de cada uno de los componentes 
+
+promedio_por_tipo = np.zeros(len(tipos_unicos))
+
+for i in range(len(tipos_unicos)):
+   promedio_por_tipo[i] = np.mean(calidad[tipos_componente == tipos_unicos[i]])
+   print("La puntuacion de calidad promedio para el ", tipos_unicos[i], "es:", promedio_por_tipo[i])
